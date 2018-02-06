140811d9871e3f95276bf7f035cdf11fffc6d842
The Aviator <abhi@xyz>
2018-02-06 07:27:59 +0530

    Implemented the async task to get JSON data of the found repositories Added internet permission in the app manifest file

--- app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -15,26 +15,44 @@
  */
 package com.example.android.datafrominternet;
 
+import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.android.datafrominternet.utilities.NetworkUtils;
 
+import java.io.IOException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    final static String GITHUB_BASE_URI = "https://api.github.com/search/repositories";
-    final static String PARAM_QUERY = "q";
-    final static String PARAM_SORT = "sort";
     EditText mSearchBoxEditText;
     TextView mUrlDisplayTextView;
     TextView mSearchResultsTextView;
+
+    /**
+     * Method to hide keyboard after the user has provided the search query
+     *
+     * @param activity object
+     */
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
      * Method that creates the search URL and displays in the url textview
      */
     void makeGithubSearchQuery() {
+
         if (TextUtils.isEmpty(mSearchBoxEditText.getText().toString())) {
             mSearchBoxEditText.setError("Please Enter a Search Query");
             return;
@@ -57,6 +76,10 @@ public class MainActivity extends AppCompatActivity {
         String searchQuery = mSearchBoxEditText.getText().toString();
         URL repoSearchUrl = NetworkUtils.buildUrl(searchQuery);
         mUrlDisplayTextView.setText(repoSearchUrl.toString());
+
+        new GithubQueryTask().execute(repoSearchUrl);
+
+        hideKeyboard(this);
     }
 
     @Override
@@ -73,5 +96,28 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public class GithubQueryTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL searchUrl = urls[0];
+            String githubSearchResults = null;
+
+            try {
+                githubSearchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            return githubSearchResults;
+        }
+
+        @Override
+        protected void onPostExecute(String githubSearchResults) {
+            if (!TextUtils.isEmpty(githubSearchResults)) {
+                mSearchResultsTextView.setText(githubSearchResults);
+            }
+        }
     }
 }
